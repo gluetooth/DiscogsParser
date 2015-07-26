6994e540f97fc6ea0bfe170861fd43159ff1d8d6
pikariop <pikariop@gmail.com>
2015-07-26 16:08:17 +0300

    Initial commit

---
+++ parser.php
@@ -0,0 +1,117 @@
+#!/usr/bin/php
+
+<?php
+
+$filename = 'records.csv';
+
+printRecords(readDiscogsCsv($filename));
+
+
+/**
+ * Read $filename as Discogs CSV dump, return records by format
+ *
+ * @param string $filename
+ * @return array
+ */
+function readDiscogsCsv($filename)
+{
+    $handle = fopen($filename, 'r');
+
+    $columns = fgetcsv($handle);
+
+    $records = array();
+
+    while (false !== ($record = fgetcsv($handle))) {
+        $records[
+            mapFormat(
+                $record[
+                    array_search('Format', $columns)
+                ]
+            )
+        ][] = array_combine($columns, $record);
+    }
+
+    fclose($handle);
+
+    return $records;
+}
+
+
+/**
+ * Echo records in desired format to stdout
+ *
+ * @param array $records
+ * @return null
+ */
+function printRecords($records)
+{
+    foreach ($records as $format => $recordsByFormat) {
+        echo "\n" . $format . "\n" . '***' . "\n";
+
+        usort($recordsByFormat, function ($a, $b) {
+            return strcasecmp($a['Artist'], $b['Artist']);
+        });
+
+        foreach ($recordsByFormat as $record) {
+            echo getArtist($record['Artist']) .
+                 ' - ' .
+                 $record['Title'] .
+                 getFormatModifiers($record['Format']) .
+                 "\n";
+        }
+    }
+
+    return null;
+}
+
+
+/**
+ * Pretty-print artist name. Strip disambiguator suffix if present.
+ *
+ * @param string $artist
+ * @return string
+ */
+function getArtist($artist)
+{
+    return preg_replace(
+        '#\ \([0-9]\)#',
+        '',
+        $artist);
+}
+
+
+/**
+ * Pretty-print record format. Map input format to available values or return it unmodified.
+ *
+ * @param string $inputFormat
+ * @return string
+ */
+function mapFormat($inputFormat)
+{
+    foreach (['7"', '10"', '12"', 'LP', 'CD', 'Cass'] as $presetFormat) {
+        if (false !== stripos($inputFormat, $presetFormat)) {
+            return $presetFormat;
+        }
+    }
+    return $inputFormat;
+}
+
+/**
+ * Prefix format with modifiers.
+ *
+ * Note that 2x pic LP is not supported with this version.
+ *
+ * @param string $format
+ * @return string
+ */
+function getFormatModifiers($format)
+{
+    foreach (['Pic', '2x'] as $modifier) {
+        if (false !== stripos($format, $modifier)) {
+            return ' ' .$modifier . mapFormat($format);
+        }
+    }
+    return null;
+}
+
+?>
